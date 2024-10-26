276b92a1b1efb68fffcb738ab732d84fdf5d4ea6
srinidhi <srinidhi799@gmail.com>
2024-10-26 17:12:54 +0530

    Inserted logo and background

--- src/Login.java
+++ src/Login.java
@@ -1,8 +1,23 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Login extends JFrame{
     Login(){
         setTitle("Automated Teller Machine");
+        setLayout(null);
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/logo.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(100,100,Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel label = new JLabel(i3);
+        label.setBounds(70, 10, 100, 100);
+        add(label);
+
+        JLabel text = new JLabel("Welcome to ATM");
+        text.setBounds(200, 40, 100, 40);
+        add(text);
+
+        getContentPane().setBackground(Color.WHITE);
+
         setSize(800, 400);
         setVisible(true);
         setLocation(350,200);

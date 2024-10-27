d4b1abd0220a30884205f334b2d9eb225d0ab43e
Ste881 <94097394+Ste881@users.noreply.github.com>
2024-10-27 22:24:50 +0530

    Added Card No and Pin feature

--- src/Login.java
+++ src/Login.java
@@ -13,8 +13,29 @@ public class Login extends JFrame{
         add(label);
 
         JLabel text = new JLabel("Welcome to ATM");
-        text.setBounds(200, 40, 100, 40);
+        text.setFont(new Font("Osward", Font.BOLD, 38));
+        text.setBounds(200, 40, 400, 40);
         add(text);
+
+        // Creating labels
+        JLabel cardno = new JLabel("Card No:");
+        cardno.setFont(new Font("Raleway", Font.BOLD, 28));
+        cardno.setBounds(120, 150, 250, 40);
+        add(cardno);
+
+        //text field for entering details
+        JTextField cardTextField = new JTextField();
+        cardTextField.setBounds(300, 150, 250, 40);
+        add(cardTextField);
+
+        JLabel pin = new JLabel("PIN:");
+        pin.setFont(new Font("Raleway", Font.BOLD, 28));
+        pin.setBounds(120, 220, 250, 40);
+        add(pin);
+
+        JTextField pinTextField = new JTextField();
+        pinTextField.setBounds(300, 220, 250, 40);
+        add(pinTextField);
 
         getContentPane().setBackground(Color.WHITE);
 
